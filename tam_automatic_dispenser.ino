5cd7e19e29c3bf30d65395e4de6f83b6f87b029a
Arath García <arathgg143@gmail.com>
2023-05-22 11:30:32 -0600

    rafactor in variables and functions names

--- tam_automatic_dispenser.ino
+++ tam_automatic_dispenser.ino
@@ -3,72 +3,70 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+const int relayPin = 10;  // Digital pin connected to the relay
+const byte ROWS = 4;      // Number of rows in the keypad
+const byte COLS = 4;      // Number of columns in the keypad
 
-const int relePin = 10;  // Pin digital al que está conectado el relé
-const byte FILAS = 4;        // Número de filas del teclado
-const byte COLUMNAS = 4;     // Número de columnas del teclado
+#define OLED_ADDR 0x3C     // I2C address of the OLED display
 
-#define OLED_ADDR 0x3C  // Dirección I2C de la pantalla OLED
+Adafruit_SSD1306 display(128, 64, &Wire, -1);  // Initialize the OLED display
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);  // Inicializar la pantalla OLED
-
-byte filasPines[FILAS] = {9, 8, 7, 6};
-byte columnasPines[COLUMNAS] = {5, 4, 3, 2};
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
 
 #define btn_on 12
 #define btn_off 13
 
-// Define la disposición de las teclas en el teclado
-char teclas[FILAS][COLUMNAS] = {
+// Define the key layout on the keypad
+char keys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
 
-Keypad teclado = Keypad(makeKeymap(teclas), filasPines, columnasPines, FILAS, COLUMNAS);
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
-  pinMode(relePin, OUTPUT);  // Configurar el pin del relé como salida
+  pinMode(relayPin, OUTPUT);  // Configure the relay pin as an output
   pinMode(btn_on, INPUT);
   pinMode(btn_off, INPUT);
-  Serial.begin(9600);        // Iniciar la comunicación serial a 9600 baudios
-
-    // Iniciar la comunicación I2C
+  Serial.begin(9600);        // Start serial communication at 9600 baud
+  
+  // Initialize I2C communication
   Wire.begin();
   
-  // Iniciar la pantalla OLED
+  // Initialize the OLED display
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
-    Serial.println("No se pudo iniciar la pantalla OLED");
+    Serial.println("Failed to initialize OLED display");
     while (1);
   }
 
-  // Borrar la pantalla
+  // Clear the display
   display.clearDisplay();
-  
-
 }
 
 void loop() {
+  char key = keypad.getKey();
 
-  char tecla = teclado.getKey();
+  if (key != NO_KEY && (key == '1' || key == '2' || key == '3' || key == '4')) {
+    Serial.println(key);  // Print the pressed key to the serial monitor
 
-  if (tecla != NO_KEY && (tecla == '1' || tecla == '2' || tecla == '3' || tecla == '4')) {
-    Serial.println(tecla);  // Imprimir la tecla presionada en el monitor serial
-
-    if(digitalRead(btn_on) == HIGH){
-      Serial.println("LLENANDO SUAVITEL");
-      digitalWrite(relePin, LOW);  // Activar el relé (conexión del circuito)
-      Serial.println("Relé encendido");  // Imprimir mensaje cuando se enciende el relé
-        // Mostrar un mensaje de bienvenida en la pantalla
+    if (digitalRead(btn_on) == HIGH) {
+      Serial.println("FILLING SOFTENER");
+      digitalWrite(relayPin, LOW);  // Activate the relay (circuit connection)
+      Serial.println("Relay turned ON");  // Print a message when the relay is turned on
+      
+      // Display a welcome message on the OLED display
       display.setTextSize(2);
       display.setTextColor(SSD1306_WHITE);
       display.setCursor(20, 10);
-      display.println("Llenando");
+      display.println("Filling");
       display.display();
-      delay(3000); 
-  }
-    digitalWrite(relePin, HIGH);   // Desactivar el relé (desconexión del circuito)
+      delay(3000);
+    }
+    
+    digitalWrite(relayPin, HIGH);  // Deactivate the relay (circuit disconnection)
     display.clearDisplay();
   }
 }
